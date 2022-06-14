bc6ae157b84b73bcda589f96aa7678b79ea46e02
unknown <=>
2022-06-14 06:11:03 -0300

    Exports modules math created for me

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const os = require('os');
 const path = require('path');
+const { add, substract, multiply, devide } = require("./math")
 
 console.log(os.type());
 console.log(os.version());
@@ -12,3 +13,8 @@ console.log(path.dirname(__filename));
 console.log(path.basename(__filename));
 console.log(path.extname(__filename));
 console.log(path.parse(__filename));
+
+console.log(add(5,10)) 
+console.log(substract(5,10)) 
+console.log(multiply(5,10)) 
+console.log(devide(5,10)) 
---
+++ math.js
@@ -0,0 +1,7 @@
+
+const add = (value , newValue) => value + newValue
+const substract = (value, newValue) => value - newValue
+const multiply = (value, newValue) => value * newValue
+const devide = (value, newValue) => value / newValue
+
+module.exports = { add, substract, multiply, devide }
