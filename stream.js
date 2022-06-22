50fa342b222b5aa8488b9de72d2d6db948bf89d5
Cristian <cristian.andrisen@gmail.com>
2022-06-22 06:39:26 -0300

    Learn write file stream

--- stream.js
+++ stream.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const rs = fs.createReadStream("./math.js", {encoding:"utf-8"});
+const ws = fs.createWriteStream("./newWriteStream");
+// rs.on("data", (dataChunk) => {
+//     console.log(dataChunk)
+// })
 
-rs.on("data", (dataChunk) => {
-    console.log(dataChunk)
-})
+rs.pipe(ws);
